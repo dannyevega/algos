ded29775ca6874eff64146e7898babae699be209
Danny Vega <dannyevega@gmail.com>
2017-10-19 09:33:45 -0700

    added some notes for stacks

--- recursion.js
+++ recursion.js
@@ -228,6 +228,45 @@ function flatten(arr){
 	return helper(arr);
 }
 
+/*
+Implement a function that will reverse a string recursively.
+reverse('abcdefg')
+=> 'gfedcba'
+*/
+function reverseString(str){
+	let result = "";
+	function helper(index){
+		if(index < 0){
+			return;
+		} else {
+			result += str[index];
+			helper(index - 1);
+		}
+	}
+	helper(str.length - 1);
+	return result;
+}
+
+/*
+Write a function that takes a string and returns all permutations of the string. Ensure that there are no duplicates in the output.
+*/
+function permutations(str){
+	let result = [];
+	if(str.length === 1){
+		result.push(str);
+		return str;
+	}
+	for(let i = 0; i < str.length; i++){
+		let current = str[i];
+		let remainingChars = str.slice(0, i) + str.slice(i + 1);
+		let innerPermutations = permutations(remainingChars);
+		for(let j = 0; j < innerPermutations.length; j++){
+			result.push(current + innerPermutations[j]);
+		}
+	}
+	return result;
+}
+
 
 
 
--- stack.js
+++ stack.js
@@ -56,3 +56,38 @@ Stack.prototype.contains = function(val) {
 Stack.prototype.getMin = function() {
 	return Object.values(this.min)[Object.values(this.min).length - 1];
 };
+
+
+
+function Stack(capacity){
+	this.capacity = capacity || Infinity;
+	this.storage = {};
+	this.count = 0;
+}
+
+Stack.prototype.push = function(value){
+	if(this.count < this.capacity){
+		this.storage[this.count] = value;
+		this.count++;
+		return this.count;
+	}
+	return "Max capacity already reached. Remove element before adding a new one.";
+}
+
+Stack.prototype.pop = function(){
+	if(this.count === 0){
+		return "No element inside the stack. Add element before poping.";
+	}
+	this.count--;
+	var value = this.storage[this.count];
+	delete this.storage[this.count];
+	return value;
+}
+
+Stack.prototype.peek = function(){
+	return this.storage[this.count - 1];
+}
+
+Stack.prototype.size = function(){
+	return this.count;
+}
